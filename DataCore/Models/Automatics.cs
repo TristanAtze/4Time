410a79b1710abbd6e95a9efe02e2c7a93b9b8471
Lorenz Kupfer <lorenz.kupfer@4sellers.de>
2025-04-17 10:43:34 +0200

    Implementation

---
+++ DataCore/Models/Automatics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Time.DataCore.Models
+{
+    internal class Automatics
+    {
+        public int AutomaticID { get; set; }
+        public string Name { get; set; }
+        public bool IsChangeable { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ DataCore/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Time.DataCore.Models
+{
+    internal class Category
+    {
+        public int CategoryID { get; set; }
+        public string Description { get; set; }
+        public bool IsWorkTime { get; set; }
+    }
+}
