1703d437182f9f874d0c9291a05e1496655b6bf2
Ben Sowieja <ben.sowieja@4sellers.de>
2025-05-15 09:02:19 +0200

    YT Blocker

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ namespace _4Time
             }
 
             string activeUser = Environment.UserName.ToLower();
+
             Updater();
             if (activeUser == "gerd.kaufmann")
             {
