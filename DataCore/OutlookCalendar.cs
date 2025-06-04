ef5ce21dc794aa12770722364fbe201b33b42e95
Noah Strauß <noah.strauss@4sellers.de>
2025-06-04 13:16:35 +0200

    add outlook import

--- DataCore/OutlookCalendar.cs
+++ DataCore/OutlookCalendar.cs
@@ -1,25 +1,27 @@
 ﻿using _4Time.DataCore.Models;
-using Microsoft.Office.Interop.Outlook;
+using Outlook = Microsoft.Office.Interop.Outlook;
 
-public class OutlookCalendar
+namespace _4Time.DataCore;
+
+public static class OutlookCalendar
 {
-    public static List<Entry> DoOutlookIntegration()
+    public static async Task<List<Entry>> DoOutlookIntegrationAsync(List<DateTime> exemptDates)
     {
-        List<Entry> entries = new List<Entry>();
-        Microsoft.Office.Interop.Outlook.Application outlook = new Microsoft.Office.Interop.Outlook.Application();
-        NameSpace nameSpace = outlook.GetNamespace("MAPI");
+        List<Entry> entries = [];
+        Outlook.Application outlook = new();
+        Outlook.NameSpace nameSpace = outlook.GetNamespace("MAPI");
 
-        MAPIFolder calendarFolder = nameSpace.GetDefaultFolder(OlDefaultFolders.olFolderCalendar);
+        Outlook.MAPIFolder calendarFolder = nameSpace.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderCalendar);
 
-        Items calendarItems = calendarFolder.Items;
-        calendarItems.IncludeRecurrences = true;
+        Outlook.Items calendarItems = calendarFolder.Items;
+        calendarItems.IncludeRecurrences = false;
         calendarItems.Sort("[Start]");
 
-        List<AppointmentItem> appointments = new List<AppointmentItem>();
+        List<Outlook.AppointmentItem> appointments = [];
 
         foreach (object item in calendarItems)
         {
-            if (item is AppointmentItem appointment)
+            if (item is Outlook.AppointmentItem appointment)
             {
                 //Überprüfung, ob der Termin länger als 1 Tag ist
                 if (!appointment.AllDayEvent)
@@ -29,8 +31,9 @@ public class OutlookCalendar
                 int dayOfWeek = (int)appointment.Start.DayOfWeek;
                 if (dayOfWeek == 0 || dayOfWeek == 6)
                     continue;
-
-                appointments.Add(appointment);
+                
+                if (!exemptDates.Contains(appointment.Start))
+                    appointments.Add(appointment);
             }
         }
 
@@ -39,11 +42,35 @@ public class OutlookCalendar
 
         appointments.Sort((a, b) => a.Start.CompareTo(b.Start));
 
-        foreach (AppointmentItem appointment in appointments)
-        {
+        int userId = (await Reader.Read<User>("User",
+                ["[UserID]"],
+                [
+                    $"[FirstName] = '{Connector.FirstName}'",
+                    $"[LastName] = '{Connector.LastName}'"
+                ])).First().UserID;
 
+        foreach (Outlook.AppointmentItem appointment in appointments)
+        {
+            foreach (int i in Enumerable.Range(0, (appointment.EndInEndTimeZone - appointment.StartInStartTimeZone).Days))
+            {
+                DateTime tmpDate = appointment.StartInStartTimeZone.AddDays(i);
+                entries.Add(new() { Start = tmpDate, End = (int)tmpDate.DayOfWeek is > 0 and < 6 ? tmpDate.AddHours(8) : tmpDate, Comment = $"[OUTLOOK-AUTO]{appointment.Subject.Replace(" ", "").Replace("[", "").Replace("]", "")}", UserID = userId });
+            }
         }
 
+        List<Category> categories = await Reader.Read<Category>("Categories", ["[CategoryID]", "[Description]"]);
+
+        entries.ForEach(x =>
+        {
+            if (x.Comment?.Contains("krank", StringComparison.CurrentCultureIgnoreCase) ?? false)
+                x.CategoryID = categories.FirstOrDefault(c => c.Description == "Krankheit")?.CategoryID ?? 6;
+            else if (x.Comment?.Contains("urlaub", StringComparison.CurrentCultureIgnoreCase) ?? false)
+                x.CategoryID = categories.FirstOrDefault(c => c.Description == "Urlaub")?.CategoryID ?? 5;
+            else if (x.Comment?.Contains("berufsschule", StringComparison.CurrentCultureIgnoreCase) ?? false)
+                x.CategoryID = categories.FirstOrDefault(c => c.Description == "Berufsschule")?.CategoryID ?? 8;
+            else
+                x.CategoryID = x.CategoryID = categories.FirstOrDefault(c => c.Description == "Planmäßige Abwesenheit")?.CategoryID ?? 7;
+        });
 
         if (calendarItems != null) System.Runtime.InteropServices.Marshal.ReleaseComObject(calendarItems);
         if (calendarFolder != null) System.Runtime.InteropServices.Marshal.ReleaseComObject(calendarFolder);
@@ -53,6 +80,6 @@ public class OutlookCalendar
         GC.Collect();
         GC.WaitForPendingFinalizers();
 
-        return new List<Entry>();
+        return entries;
     }
 }
